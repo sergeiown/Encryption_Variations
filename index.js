63e74dddd38cf6928b4af375675ab8822d1102fa
Serhii Myshko <sergeiown@gmail.com>
2024-04-22 18:53:54 +0300

    add cookies

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ const textInput = document.getElementById('textInput');
 const encryptionMethodSelect = document.getElementById('encryptionMethod');
 const outputTextarea = document.getElementById('output');
 
+window.addEventListener('load', () => {
+    const storedText = getCookie('storedText');
+    if (storedText) {
+        textInput.value = storedText;
+        processText();
+    }
+});
+
 textInput.addEventListener('input', () => {
+    processText();
+});
+
+encryptionMethodSelect.addEventListener('change', () => {
+    processText();
+});
+
+function processText() {
     const text = textInput.value.trim();
     const encryptionMethod = encryptionMethodSelect.value;
+    saveCookie('storedText', text);
 
     if (encryptionMethod === 'base64') {
         processTextBase64(text);
     } else if (encryptionMethod === 'xor') {
         processTextXOR(text);
     }
-});
+}
 
 function processTextBase64(text) {
     const isEncoded = isBase64(text);
@@ -87,4 +104,27 @@ function base64EncodeUnicode(str) {
             return String.fromCharCode('0x' + p1);
         })
     );
+}
+
+function saveCookie(name, value, days = 365) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = name + '=' + value + ';' + expires + ';path=/';
+}
+
+function getCookie(name) {
+    const cookieName = name + '=';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return '';
 }
